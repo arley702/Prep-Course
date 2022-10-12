50be4c97514d3a423192f3776b51e0f367a2996d
Anderson <arley.alba702@gmail.com>
2022-10-12 01:22:07 -0500

    firts commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -3,6 +3,7 @@
 // Crea una variable "string", puede contener lo que quieras:
 const nuevaString = null;
 
+
 const PruebaComit = String;
 
 // Crea una variable numérica, puede ser cualquier número:
